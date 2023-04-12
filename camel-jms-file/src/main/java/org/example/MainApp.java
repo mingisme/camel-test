f49aa2b70e261f17933ff4677cd0269344f77c58
ming.wang4 <ming.wang4@envision-digital.com>
2023-04-12 22:54:07 +0800

    camel-jms-file

---
+++ camel-jms-file/src/main/java/org/example/MainApp.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+import javax.jms.ConnectionFactory;
+
+public class MainApp {
+    public static void main(String args[]) throws Exception {
+
+        CamelContext context = new DefaultCamelContext();
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
+        context.addComponent("test-jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+        context.addRoutes(new RouteBuilder() {
+            public void configure() {
+                from("test-jms:queue:test.queue").to("file://test");
+            }
+        });
+        context.start();
+        ProducerTemplate template = context.createProducerTemplate();
+        for (int i = 0; i < 10; i++) {
+            template.sendBody("test-jms:queue:test.queue", "Test Message: " + i);
+        }
+        Thread.sleep(1000);
+        context.stop();
+    }
+}
